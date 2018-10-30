459be37150b42131266a71872b824a9156f6d059
Meatyflesh <robinklein97@gmail.com>
2018-10-30 09:34:21 +0100

    Updated help command

--- src/DiscordBot/Modules/HelpModule.cs
+++ src/DiscordBot/Modules/HelpModule.cs
@@ -11,8 +11,11 @@ namespace DiscordBot.Modules
                 $"```\n" +
                 $"info - Display some information about this bot.\n" +
                 $"help - Display this message.\n" +
-                $"Prompt - Display the current Prompt.\n" +
-                $"Respond - SEND THIS IN A DM - Respond to the prompt.\n" +
+                $"prompt - Display the current Prompt.\n" +
+                $"respond - SEND THIS IN A DM - Respond to the prompt.\n" +
+                $"vote - SEND THIS IN A DM - Vote on a voting screen\n" +
+                $"interested - Gives you the Virtual role, use if interested in RPBot updates\n" +
+                $"review - Gives you the Reviewer role, use if interested in revieuweing responses.\n" +
                 $"tr_me [optional opacity 0-100] - get a new tr_endy profile picture\n" +
                 $"```");
     }
